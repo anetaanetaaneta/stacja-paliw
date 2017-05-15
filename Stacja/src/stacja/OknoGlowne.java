0007caaba2163ef9532d1616d8688640563ab2d4
katarzynakamieniorz <asleep@onet.eu>
2017-05-15 17:33:46 +0200

    15.05 17.30

--- Stacja/src/stacja/OknoGlowne.java
+++ Stacja/src/stacja/OknoGlowne.java
@@ -27,21 +27,56 @@ public class OknoGlowne extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        bLogowanie = new javax.swing.JButton();
+        bRejestracja = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        bLogowanie.setText("Logowanie");
+        bLogowanie.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bLogowanieActionPerformed(evt);
+            }
+        });
+
+        bRejestracja.setText("Rejestracja");
+        bRejestracja.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bRejestracjaActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(219, 219, 219)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(bRejestracja, javax.swing.GroupLayout.DEFAULT_SIZE, 297, Short.MAX_VALUE)
+                    .addComponent(bLogowanie, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(165, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(51, 51, 51)
+                .addComponent(bLogowanie, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(bRejestracja, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(114, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void bLogowanieActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bLogowanieActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_bLogowanieActionPerformed
+
+    private void bRejestracjaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bRejestracjaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_bRejestracjaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +114,7 @@ public class OknoGlowne extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton bLogowanie;
+    private javax.swing.JButton bRejestracja;
     // End of variables declaration//GEN-END:variables
 }
